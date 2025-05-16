482e6a9f50f0ae339300402cbce3189913da9d71
Mateo Ramirez Hernandez <133136624+SrRexxo@users.noreply.github.com>
2025-05-15 20:18:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -86,3 +86,14 @@ with col3:
         st.plotly_chart(px.line(uv_df, x="time", y="uv_index", title="Índice UV"), use_container_width=True)
     else:
         st.info("Sin datos de índice UV en este rango.")
+
+st.title("Dashboard IoT del Clima")
+
+st.markdown(
+    """
+    ### Ver panel de clima
+    Debido a políticas de seguridad, el panel no puede mostrarse aquí directamente.
+    [Haz clic aquí para abrir el panel en una nueva pestaña.](https://miguelcmo.grafana.net/d-solo/aehqn58kr54aof/home-iot-weather-conditions?orgId=1&from=1747328396681&to=1747349996681&timezone=browser&refresh=10s&panelId=3&__feature.dashboardSceneSolo)
+    """,
+    unsafe_allow_html=True
+)
